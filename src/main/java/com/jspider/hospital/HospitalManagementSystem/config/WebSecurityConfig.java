e658b83877dc91fd3d088126b00d530caf96fde4
ayushsahu007 <ayushsahu007ay@gmail.com>
2026-03-01 01:22:01 +0530

    Jwt token verification complete

--- src/main/java/com/jspider/hospital/HospitalManagementSystem/config/WebSecurityConfig.java
+++ src/main/java/com/jspider/hospital/HospitalManagementSystem/config/WebSecurityConfig.java
@@ -1,24 +1,35 @@
 package com.jspider.hospital.HospitalManagementSystem.config;
 
+import com.jspider.hospital.HospitalManagementSystem.security.JwtAuthFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@AllArgsConstructor
 public class WebSecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                  .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig ->
+                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**","/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
+                                .anyRequest().authenticated()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
                 )
-                .httpBasic(Customizer.withDefaults());
+
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 }
